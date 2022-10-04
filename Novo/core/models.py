7b933b19c0c8b75d83f18d095864a1e4e1d566ad
luiza-omena <99267967+luiza-omena@users.noreply.github.com>
2022-10-04 16:56:40 -0300

    Models

--- Novo/core/models.py
+++ Novo/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    nick_name = models.CharField(max_length=50)
+    email = models.EmailField(blank=False, max_length=100, default=None)
+    password = models.CharField(max_length=100)
+    region = models.CharField(max_length=20)
+
+    def str(self):
+        return self.name
+
+class Receitas(models.Model):
+    titulo = models.CharField(max_length=50)
+    ingredientes = models.CharField(max_length=300)
+    descricao = models.TextField()
+
+    def str(self):
+        return self.titulo
+
+class Indicacoes(models.Model):
+    descricao2 = models.TextField()
+    tipo = models.CharField(max_length=50)
+
+    def str(self):
+        return self.decricao2
+
